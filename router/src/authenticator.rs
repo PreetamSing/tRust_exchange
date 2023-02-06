08787c7609a2c759c6ed3ea1c31f9bbc2a16c74b
PreetamSing <rathoresinghpreetam@gmail.com>
2023-02-06 22:26:57 +0530

    WIP: verify jwt token using public key.

--- router/src/authenticator.rs
+++ router/src/authenticator.rs
@@ -1,25 +1,29 @@
+#![allow(dead_code)] // TODO: remove this
+
 use apollo_router::plugin::Plugin;
 use apollo_router::plugin::PluginInit;
 use apollo_router::register_plugin;
 use apollo_router::services::execution;
 use apollo_router::services::subgraph;
 use apollo_router::services::supergraph;
+use jwt_simple::prelude::*;
 use schemars::JsonSchema;
 use serde::Deserialize;
+use std::fs;
 use tower::BoxError;
 use tower::ServiceBuilder;
 use tower::ServiceExt;
 
 #[derive(Debug)]
 struct Authenticator {
-    #[allow(dead_code)] // TODO: remove this
     configuration: Conf,
+    pub_key: RS256PublicKey,
 }
 
 #[derive(Debug, Default, Deserialize, JsonSchema)]
 struct Conf {
     // Put your plugin configuration here. It will automatically be deserialized from JSON.
-    rsa_pub_key: String, // RSA public key against which JWT shall be verified.
+    pub_key_file: String, // RSA public key against which JWT shall be verified.
 }
 
 #[async_trait::async_trait]
@@ -27,23 +31,28 @@ impl Plugin for Authenticator {
     type Config = Conf;
 
     async fn new(init: PluginInit<Self::Config>) -> Result<Self, BoxError> {
-        println!("Initializing Plugin with rsa_pub_key: {}", &init.config.rsa_pub_key);
+        let pub_key_string = fs::read_to_string(&init.config.pub_key_file)?;
+        let pub_key = RS256PublicKey::from_pem(&pub_key_string)?;
         Ok(Authenticator {
             configuration: init.config,
+            pub_key,
         })
     }
 
     fn supergraph_service(&self, service: supergraph::BoxService) -> supergraph::BoxService {
         println!("Inside `supergraph_service`");
+        let pub_key = self.pub_key.clone();
         // Always use service builder to compose your plugins.
         // It provides off the shelf building blocks for your plugin.
         ServiceBuilder::new()
-            .map_request(|mut req: supergraph::Request| {
+            .map_request(move |mut req: supergraph::Request| {
                 let mut is_valid = false; // Keep valid token indicator to false initially and only set to true once validated.
                 if let Some(bearer) = req.supergraph_request.headers().get("authorization") {
-                    let verify_token = |_token: &str| {
-                        // TODO: verify token using rsa_pub_key.
-                        true
+                    let verify_token = |token: &str| {
+                        let _ = dbg!(&pub_key);
+                        let _ = dbg!(pub_key.verify_token::<NoCustomClaims>(&token, None));
+                        // TODO: verify authenticity of JWT token using public key.
+                        todo!("verify_token")
                     };
 
                     let token = bearer.to_str().unwrap_or("").split_whitespace().skip(1).next();
